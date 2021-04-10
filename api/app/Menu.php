c6ff34b5c5e71e98c60659ef2c899cb7df32ae18
niemet0502 <vincentmarius8@gmail.com>
2021-04-11 00:34:18 +0200

    main :: created migration and Model file for Menu

---
+++ api/app/Menu.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'name', 'image', 'description'
+    ];
+}
