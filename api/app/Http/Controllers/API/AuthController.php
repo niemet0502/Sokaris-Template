df3874e82c8019bcff20da68308e360d370c50a3
niemet0502 <vincentmarius8@gmail.com>
2021-04-19 23:30:35 +0200

    main :: init website project

--- api/app/Http/Controllers/API/AuthController.php
+++ api/app/Http/Controllers/API/AuthController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 
 
-class AuthController extends Controller
+class AuthController extends BaseController
 {
 
     /**
--- api/routes/api.php
+++ api/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,7 +22,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware('auth:api')->group( function ()  {
-    Route::resource('menus', MenuController::class);
-});
+    Route::resource('menus',  API\MenuController::class);
+
 
