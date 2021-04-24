282a8415576a9fccce4c683413d9ec840e5debe1
niemet0502 <vincentmarius8@gmail.com>
2021-04-25 00:52:38 +0200

    main :: added relationship metho for prodct model

--- api/app/Models/Product.php
+++ api/app/Models/Product.php
@@ -15,4 +15,21 @@ class Product extends Model
         'description',
         'price'
     ];
+
+     /**
+     * Get the images for the product.
+     */
+    public function images()
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+     /**
+     * Get the menu that owns the product.
+     */
+    public function menu()
+    {
+        return $this->belongsTo(Menu::class);
+    }
+
 }
