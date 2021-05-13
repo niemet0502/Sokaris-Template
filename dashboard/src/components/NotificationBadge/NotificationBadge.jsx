692eadf5973aa64cb6fce3bcb37e2dfd304a5d82
niemet0502 <vincentmarius8@gmail.com>
2021-05-13 20:15:13 +0200

    main :: create NotificationBadge component

---
+++ dashboard/src/components/NotificationBadge/NotificationBadge.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./NotificationBadge.css"
+function NotificationBadge() {
+  return (
+    <div>
+      <h1>Notification Badge</h1>
+    </div>
+  )
+}
+
+export default NotificationBadge
