33c889604864de5a770f7c78a366aa43632fe725
niemet0502 <vincentmarius8@gmail.com>
2021-05-15 15:42:49 +0200

    displayed user info on topBar

--- dashboard/src/components/Topbar/Topbar.jsx
+++ dashboard/src/components/Topbar/Topbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import "./Topbar.css"
+import user from "../../assets/images/user.jpg"
 function Topbar() {
   return (
-    <div>
-      
+    <div className="Topbar">
+      <div className="Topbar__search">
+        <input type="text" name="" placeholder="Search here" id="" />
+      </div>
+      <div className="Topbar__badge">
+        T
+      </div>
+      <div className="Topbar__user">
+        Hello, &nbsp; <span>Marius</span> 
+        <img src={user} alt="" />
+      </div> 
     </div>
   )
 }
