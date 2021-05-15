0663c4967064c357f81965841fbceb528aaef4b8
niemet0502 <vincentmarius8@gmail.com>
2021-05-15 16:50:18 +0200

    dispalyed logo on sidebar

--- dashboard/src/components/Sidebar/Sidebar.jsx
+++ dashboard/src/components/Sidebar/Sidebar.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 import "./Sidebar.css"
 function Sidebar() {
   return (
-    <div className="sidebar">
-      
+    <div className="Sidebar">
+      <div className="Sidebar_logoContainer">
+        <h1> Sedap <span>.</span></h1>
+        <h6>Modern Admin Dashboard</h6>
+      </div>
     </div>
   )
 }
